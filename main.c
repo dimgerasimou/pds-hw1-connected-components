b6bff8a48b2ef52a4ab3524b3037563ac68b1120
Dimitris Gerasimou <gerasimou-gh@posteo.gr>
2025-11-04 20:58:52 +0200

    refactor: Refactored main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "mat_read.h"
 
 int main(int argc, char *argv[]) {
+
     if (argc != 2) {
         printf("%s: invalid arguments", argv[0]);
         return 1;
@@ -13,10 +14,12 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Failed to load sparse array\n");
         return 1;
     }
-for (unsigned int i=0; i<sparse->ncol; i++){
-    for(unsigned int j= sparse->col_ptr[i]; j<sparse->col_ptr[i+1];j++)
-        printf("(%5d,%d)  %.0lf\n",sparse->row_idx[j] + 1, i + 1, sparse->val[j]);
-}
+
+    for (unsigned int i=0; i<sparse->ncol; i++){
+        for(unsigned int j= sparse->col_ptr[i]; j<sparse->col_ptr[i+1];j++) {
+            printf("(%5d,%d)  %.0lf\n",sparse->row_idx[j] + 1, i + 1, sparse->val[j]);
+        }
+    }
 
     free_sparse_matrix(sparse);
     return 0;
